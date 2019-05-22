95c4d07180be15886e9406bf7eb73fb38ada525c
Sorgelig <pour.garbage@gmail.com>
2019-05-23 02:46:26 +0800

    vt: reduce from 63 to 9 ttys.

--- include/uapi/linux/vt.h
+++ include/uapi/linux/vt.h
@@ -8,7 +8,7 @@
  * resizing).
  */
 #define MIN_NR_CONSOLES 1       /* must be at least 1 */
-#define MAX_NR_CONSOLES	63	/* serial lines start at 64 */
+#define MAX_NR_CONSOLES	9	/* serial lines start at 64 */
 		/* Note: the ioctl VT_GETSTATE does not work for
 		   consoles 16 and higher (since it returns a short) */
 
